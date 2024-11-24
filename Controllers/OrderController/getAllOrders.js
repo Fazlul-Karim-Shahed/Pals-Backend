4a579df25a20de6fed62bfbbb80c16fb76506ca5
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-11-24 13:07:14 +0600

    bug fix

--- Controllers/OrderController/getAllOrders.js
+++ Controllers/OrderController/getAllOrders.js
@@ -1,7 +1,8 @@
 const { OrderModel } = require("../../Models/OrderModel");
 
 const getAllOrders = async (req, res) => {
-    let query = {};
+    
+    let query = { ...req.body };
 
     if (req.body.day && !isNaN(req.body.day)) {
         query = {
@@ -9,18 +10,22 @@ const getAllOrders = async (req, res) => {
             orderDate: { $gte: new Date(Date.now() - req.body.day * 24 * 60 * 60 * 1000) }
         };
     }
-    else if(req.body.startDate) {
+    else if (req.body.startDate) {
         query = {
             ...query,
             orderDate: { $gte: new Date(req.body.startDate), $lt: req.body.endDate ? new Date(req.body.endDate) : new Date(Date.now()) }
         };
     }
 
+    console.log(query);
+
     let order = await OrderModel.find(query)
         .sort({ orderDate: -1 })
         .populate({ path: 'orderList.productId', model: 'Product' }).populate(['deliveryMethod']);
 
-    if (order.length !== 0) {
+    console.log(order);
+    
+    if (order.length != 0) {
         res.status(200).send({ message: 'All orders within the specified date range', error: false, data: order });
     } else {
         res.send({ message: 'No orders found within the specified date range', error: true });
