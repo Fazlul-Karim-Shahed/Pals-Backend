fa196e9a81568e84d13cc81e5d65f90743cbea32
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2025-06-19 22:45:57 +0600

    event update

--- Controllers/EventControllers/getAllEvents.js
+++ Controllers/EventControllers/getAllEvents.js
@@ -3,18 +3,22 @@ const { EventModel } = require("../../Models/EventModel");
 const getAllEvents = async (req, res) => {
     console.log(req.params);
 
-    const today = new Date();
+    const now = new Date();
+    const startOfToday = new Date(now.setHours(0, 0, 0, 0));
+    const endOfToday = new Date(now.setHours(23, 59, 59, 999));
 
-    let event = await EventModel.find(
-        req.params && req.params.today != 'undefined'
-            ? {
-                  startDate: { $lte: today },
-                  endDate: { $gte: today },
-              }
-            : {}
-    );
+    let filter = {};
 
-    if (event.length != 0) {
+    if (req.params && req.params.today !== "undefined") {
+        filter = {
+            startDate: { $lte: endOfToday },
+            endDate: { $gte: startOfToday },
+        };
+    }
+
+    const event = await EventModel.find(filter);
+
+    if (event.length !== 0) {
         res.status(200).send({ message: "All event", error: false, data: event });
     } else {
         res.send({ message: "No event found", error: true });
